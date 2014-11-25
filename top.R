052aed1bc4d13373a3bf5dbb2b5cc10e8db0d73c
OssiB <ossi.bister@gmail.com>
2014-11-25 13:22:49 +0200

    top.R extra ) removed

--- top.R
+++ top.R
@@ -3,5 +3,5 @@ dataTables <- readHTMLTable(wikiUrl)
 statePopulation <- tables[4]
 top20  <-  data[c(1:21),c(3,4)]
 top20[,2] <- gsub(",","",top20[,2])
-top20[,2] <- as.numeric(top20[,2]))
+top20[,2] <- as.numeric(top20[,2])
 write.csv(top20,"top.csv")
