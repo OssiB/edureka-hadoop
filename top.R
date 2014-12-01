592556f71b61e80662dd8de73a9bedff4def747a
OssiB <ossi.bister@gmail.com>
2014-12-01 10:50:47 +0200

    Downloads data from StackExchange

--- top.R
+++ top.R
@@ -1,7 +1,15 @@
-wikiUrl  <- "http://en.wikipedia.org/wiki/List_of_U.S._states_and_territories_by_population"
+wikiUrl  <- "http://en.wikipedia.org/wiki/List_of_states_and_territories_of_the_United_States"
 dataTables <- readHTMLTable(wikiUrl)
-statePopulation <- tables[4]
-top20  <-  data[c(1:21),c(3,4)]
-top20[,2] <- gsub(",","",top20[,2])
-top20[,2] <- as.numeric(top20[,2])
+statePopulation <- dataTables[[1]]
+statePopulation[,6] <- gsub(",","",statePopulation[,6])
+statePopulation[,6] <-as.numeric(statePopulation[,6])
+names(statePopulation)[6] <- "Population"
+statePopulation <- statePopulation[order(-statePopulation$Population),]
+top20<- statePopulation[1:20,c(2,6)]
 write.csv(top20,"top.csv")
+
+states <-top20[,1]
+for (state in states){
+	stateUrl<-paste("http://data.stackexchange.com/stackoverflow/csv/326410?state=",state,sep="")
+	download.file(stateUrl,paste(state,".csv",sep=""),method = "curl")
+}
